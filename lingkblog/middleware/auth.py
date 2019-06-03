a232feec437da4fc6fb036b569ca7d32d5c0ce15
JalanJiang <448300947@qq.com>
2019-06-03 23:26:43 +0800

    style(*): 修改类名命名风格

--- lingkblog/middleware/auth.py
+++ lingkblog/middleware/auth.py
@@ -3,15 +3,15 @@
 @version: v1.0
 @Author: JalanJiang
 @Date: 2019-06-03 00:13:35
-@LastEditTime: 2019-06-03 00:19:07
+@LastEditTime: 2019-06-03 23:25:42
 '''
 
-class auth_middleware():
+class AuthMiddleware():
     def __init__(self, wsgi_app):
         self.wsgi_app = wsgi_app
 
     def __call__(self, *args, **kwargs):
         print('before')
         res = self.wsgi_app(*args, **kwargs)
-        print('after')å
+        print('after')
         return res
--- run.py
+++ run.py
@@ -3,12 +3,12 @@
 @version: v1.0
 @Author: JalanJiang
 @Date: 2019-06-02 01:43:27
-@LastEditTime: 2019-06-03 00:18:15
+@LastEditTime: 2019-06-03 23:25:53
 '''
 from lingkblog import create_app
 from lingkblog.middleware import auth
 
 
 app = create_app()
-app.wsgi_app = auth.auth_middleware(app.wsgi_app)
+app.wsgi_app = auth.AuthMiddleware(app.wsgi_app)
 app.run()
