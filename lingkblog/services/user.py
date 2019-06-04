31c292fe5b53aa6da9725c1f53ebb492405b4daf
JalanJiang <448300947@qq.com>
2019-06-05 01:24:44 +0800

    feat(user): 用户登录逻辑增加验证器

--- lingkblog/services/user.py
+++ lingkblog/services/user.py
@@ -1,6 +1,15 @@
+'''
+@Descripttion: 
+@version: v1.0
+@Author: JalanJiang
+@Date: 2019-06-03 23:03:33
+@LastEditTime: 2019-06-05 01:21:39
+'''
 from lingkblog.common.jwt_auth import JWTAuth
 from lingkblog.services.base import Base as BaseService
 import time
+from lingkblog.common.validators.login_form import LoginForm
+from flask_api import status
 
 
 class User(BaseService):
@@ -9,13 +18,23 @@ class User(BaseService):
         self.request = request
 
     def login(self):
-        username = self.request.form['username']
-        password = self.request.form['password']
+        '''
+        @descripttion: /admin-api/users/token 用户登录方法
+        @param body email 用户邮箱
+        @param body password 用户密码
+        @return: 
+        '''
+        # 请求参数验证
+        login_form = LoginForm(self.request.form)
+        if login_form.validate() == False:
+            return self.return_error(status.HTTP_400_BAD_REQUEST, login_form.errors)
 
-        # todo: 连接数据表验证
+        email = self.request.form['email']
+        password = self.request.form['password']
         
-        # 暂为测试用
-        if username == 'admin' and password == 'admin':
+        # TODO: 暂为测试用，后续补充验证逻辑
+        if email == 'yahaha@gmail.com' and password == 'test@123':
+            # 登录成功
             user_id = 1
             updated_at = int(time.time())
             access_token = JWTAuth.encode_access_token(user_id, updated_at)
@@ -24,5 +43,6 @@ class User(BaseService):
                 'expire_at': 7200
             }
             return self.return_success(response_data)
-            
-
+        else:
+            # 登录失败
+            return self.return_error(status.HTTP_400_BAD_REQUEST, '登录失败')
