2fc27b63ac5fe30cc1ee274ef96196eaf9da24c3
JalanJiang <448300947@qq.com>
2019-06-05 18:54:31 +0800

    style(*): 删除无用注释

--- lingkblog/routes/admin/user.py
+++ lingkblog/routes/admin/user.py
@@ -3,10 +3,10 @@
 @version: v1.0
 @Author: JalanJiang
 @Date: 2019-06-02 15:08:20
-@LastEditTime: 2019-06-04 00:24:49
+@LastEditTime: 2019-06-05 18:31:12
 '''
 from flask import Blueprint, request
-from lingkblog.services.user import User as UserService
+from lingkblog.services.admin.user import User as UserService
 
 bp = Blueprint("token", __name__, url_prefix="/admin-api/users")
 
@@ -14,11 +14,10 @@ bp = Blueprint("token", __name__, url_prefix="/admin-api/users")
 @bp.route("/token", methods=['POST', 'GET', 'DELETE'])
 def account_token():
     if request.method == 'POST':
+        # 用户登录操作
         user = UserService(request)
         return user.login()
-        # pass
     if request.method == 'PUT':
         pass
     if request.method == 'DELETE':
         pass
-    # return "Hello, LingKBlog Users!"
