7497089d22f81d9630ef6f95e10161faadec96b7
JalanJiang <448300947@qq.com>
2019-06-05 15:27:57 +0800

    fix(common): 修改 jwt 工具中读取配置的错误方法
    读取配置不应该直接从 config.py 中引入；
    正确读取方法为：app.config['key']

--- lingkblog/common/jwt_auth.py
+++ lingkblog/common/jwt_auth.py
@@ -3,10 +3,11 @@
 @version: v1.0
 @Author: JalanJiang
 @Date: 2019-06-03 23:39:17
-@LastEditTime: 2019-06-03 23:52:15
+@LastEditTime: 2019-06-05 14:21:03
 '''
 import jwt
 import datetime
+from flask import current_app as app
 from config import JWT_LEEWAY, SECRET_KEY
 
 
@@ -15,14 +16,15 @@ class JWTAuth():
     def encode_access_token(user_id, updated_at):
         '''
         @descripttion: 加密获取 access_token
-        @param {type} 
+        @param integer user_id 用户ID
+        @param integer updated_at 更新时间戳
         @return: 
         '''
         try:
             payload = {
                 'exp':
                 datetime.datetime.utcnow() +
-                datetime.timedelta(seconds=JWT_LEEWAY),
+                datetime.timedelta(seconds=app.config['JWT_LEEWAY']),
                 'iat':
                 datetime.datetime.utcnow(),
                 'iss':
@@ -32,6 +34,6 @@ class JWTAuth():
                     'updated_at': updated_at
                 }
             }
-            return jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+            return jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
         except Exception as e:
             return e
