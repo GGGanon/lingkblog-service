ce7c6830675e5e5954250372435d2b90d3282af3
JalanJiang <448300947@qq.com>
2019-06-05 15:19:53 +0800

    refactor(app): 入口文件优化
    1. 修改入口文件名称 run.py 为 app.py，from app 比 from run 看起来顺眼些
    2. 入口文件中的 app.run() 放入 if 语句中，避免包引入时执行 run
    3. 加载配置文件方法改为在 create_app 工厂方法中执行
    4. SQLAlchemy 初始化 db

---
+++ app.py
@@ -0,0 +1,44 @@
+'''
+@Descripttion: 应用入口
+@version: v1.0
+@Author: JalanJiang
+@Date: 2019-06-02 01:43:27
+@LastEditTime: 2019-06-05 15:06:47
+'''
+from lingkblog import create_app
+from lingkblog.middleware import auth
+from flask_sqlalchemy import SQLAlchemy
+# from config import DATABASE
+from flask import g
+import sqlite3
+
+
+# g.db = SQLAlchemy(app)
+
+# def connect_db():
+#     '''
+#     @description: 连接 SQLite
+#     '''
+#     return sqlite3.connect(DATABASE)
+
+# @app.before_request
+# def before_request():
+#     '''
+#     @description: 请求前连接数据库
+#     '''
+#     g.db = SQLAlchemy(app)
+    
+
+# @app.teardown_request
+# def teardown_request(exception):
+#     '''
+#     @description: 请求结束后关闭数据库连接
+#     '''
+#     if hasattr(g.db, 'db'):
+#         g.db.close()
+
+app = create_app()
+db = SQLAlchemy(app)
+
+if __name__ == "__main__":
+    app.run()
