ccbc7b5a3712f4927b8027a2e4d23d12065a4ba5
JalanJiang <448300947@qq.com>
2019-06-05 15:16:54 +0800

    feat(config): 新增 SQLite 相关配置

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
+# APP
 FLASK_ENV = 'development'
 DEBUG = True
 
 # JWT
 SECRET_KEY = '7PXsHcHGfa4e3kEs8Rvcv8ymjI0UeauX'
 JWT_LEEWAY = 604800
+
+# DB
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/jalan/lingkblog_test.db'
+DATABASE = 'sqlite:////Users/jalan/lingkblog_test.db'
