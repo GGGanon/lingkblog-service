fa8fa567785a97a9d7bc867e6a81fa0cf56fda83
JalanJiang <448300947@qq.com>
2019-06-06 15:03:26 +0800

    feat(config): 错误码补充

--- lingkblog/config/error/code.py
+++ lingkblog/config/error/code.py
@@ -3,11 +3,18 @@
 @Version: v1.0
 @Author: JalanJiang
 @Date: 2019-06-06 12:37:42
-@LastEditTime: 2019-06-06 14:07:57
+@LastEditTime: 2019-06-06 14:55:47
 '''
 
 error_code = {
-    'validate_err': 40001100,
-    # account 模块
-    'email_already_exists': 40003001,
+    'validate_err': 10000,
+    # 授权相关
+    'login_fail': 10001, # 登录失败
+    'invalid_token': 10002,
+    'expired_token': 10006,
+
+    # user模块 - 11
+    'account_not_found': 11001,
+    # account模块 - 12
+    'email_already_exist': 12001,
 }
