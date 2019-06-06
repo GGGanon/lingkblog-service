c5060feb2690a5f6e068b57c14c568a3e26799bf
JalanJiang <448300947@qq.com>
2019-06-06 14:19:50 +0800

    style(*): 代码规范调整

--- lingkblog/services/admin/account.py
+++ lingkblog/services/admin/account.py
@@ -3,23 +3,27 @@ from lingkblog.common.validators.store_account_form import StoreAccountForm
 from lingkblog.models.account import Account as AccountModel
 from lingkblog import db
 from lingkblog.common.bcrypt_password import BcryptPassword
+from lingkblog.exceptions.api_exception import APIException
 
 from flask_api import status as http_status
 
 
 class Account(BaseService):
+    
     def store(self):
         store_account_form = StoreAccountForm(self.request.form)
-        if store_account_form.validate() == False:
-            return self.return_error(http_status.HTTP_400_BAD_REQUEST, store_account_form.errors)
+        if not store_account_form.validate():
+            raise APIException(err_msg=store_account_form.errors, err_key='validate_err')
 
         name     = self.request.form['name']
         email    = self.request.form['email']
-        # TODO: 密码加密
         password = BcryptPassword.encode_password(self.request.form['password'])
-        # TODO: 强制类型转换？
-        role_id = self.request.form['role_id']
-        status  = self.request.form['status']
+        role_id  = self.request.form['role_id']
+        status   = self.request.form['status']
+
+        # 判断 email 是否已存在
+        if AccountModel.query.filter_by(email=email).first():
+            raise APIException(err_key='email_already_exists')
 
         # 新增数据库入
         account_obj = AccountModel(name=name, email=email, password=password, role_id=role_id, status=status)
--- lingkblog/services/admin/user.py
+++ lingkblog/services/admin/user.py
@@ -3,7 +3,7 @@
 @version: v1.0
 @Author: JalanJiang
 @Date: 2019-06-03 23:03:33
-@LastEditTime: 2019-06-06 01:34:15
+@LastEditTime: 2019-06-06 14:16:22
 '''
 from lingkblog.common.jwt_auth import JWTAuth
 from lingkblog.services.base import Base as BaseService
@@ -16,9 +16,6 @@ import time
 
 
 class User(BaseService):
-    # def __init__(self, request):
-    #     super().__init__()
-    #     self.request = request
 
     def login(self):
         '''
@@ -29,7 +26,7 @@ class User(BaseService):
         '''
         # 请求参数验证
         login_form = LoginForm(self.request.form)
-        if login_form.validate() == False:
+        if not login_form.validate():
             return self.return_error(status.HTTP_400_BAD_REQUEST, login_form.errors)
 
         email    = self.request.form['email']
