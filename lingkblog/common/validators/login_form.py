a5fe94a5682b29d8dfdb21f284491aabc4a313b9
JalanJiang <448300947@qq.com>
2019-06-06 13:12:38 +0800

    feat(validator): 验证器补充错误提示信息

--- lingkblog/common/validators/login_form.py
+++ lingkblog/common/validators/login_form.py
@@ -3,13 +3,13 @@
 @version: v1.0
 @Author: JalanJiang
 @Date: 2019-06-05 00:52:02
-@LastEditTime: 2019-06-05 01:15:40
+@LastEditTime: 2019-06-06 11:54:02
 '''
 from wtforms import Form, StringField, TextField
 from wtforms.validators import Email, input_required
 
 
 class LoginForm(Form):
-    email = StringField(validators=[Email(), input_required()])
+    email = StringField(validators=[Email(message='请输入有效的邮箱地址'), input_required(message='邮箱不能为空')])
     # TODO：密码长度限制
-    password = StringField(validators=[input_required()])
+    password = StringField(validators=[input_required('密码不能为空')])
--- lingkblog/common/validators/store_account_form.py
+++ lingkblog/common/validators/store_account_form.py
@@ -3,17 +3,17 @@
 @version: v1.0
 @Author: JalanJiang
 @Date: 2019-06-05 23:02:07
-@LastEditTime: 2019-06-06 01:01:45
+@LastEditTime: 2019-06-06 12:00:13
 '''
 from wtforms import Form, StringField, IntegerField
-from wtforms.validators import input_required, Email
+from wtforms.validators import input_required, Email, Length
 
 
 class StoreAccountForm(Form):
-    name = StringField(validators=[input_required()])
-    email = StringField(validators=[Email(), input_required()])
+    name = StringField(validators=[input_required(message='用户名不能为空')])
+    email = StringField(validators=[Email(message='请输入有效的邮箱地址'), input_required(message='邮箱不能为空')])
     # TODO: 密码长度限制
-    password = StringField(validators=[input_required()])
-    role_id = IntegerField(validators=[input_required()])
+    password = StringField(validators=[input_required(message='密码不能为空'), Length(min=8, max=15, message='密码长度必须为 8~15 位')])
+    role_id = IntegerField(validators=[input_required(message='用户角色不能为空')])
     # TODO: 限制 0 或 1
     status = IntegerField(validators=[input_required()])
