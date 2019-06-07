41940cdd98683fa782449312f3ccb16eacbc217b
JalanJiang <448300947@qq.com>
2019-06-07 17:42:50 +0800

    fix(model): 文章表新增字段

--- lingkblog/models/article.py
+++ lingkblog/models/article.py
@@ -3,7 +3,7 @@
 @version: v1.0
 @Author: JalanJiang
 @Date: 2019-06-07 11:12:59
-@LastEditTime: 2019-06-07 12:52:21
+@LastEditTime: 2019-06-07 17:38:40
 '''
 from sqlalchemy import Column, Integer, String, Text, JSON
 
@@ -12,16 +12,18 @@ from lingkblog.models.base import Base
 
 
 class Article(Base, db.Model):
-    __tablename__ = 'articles'
-    id            = Column(Integer, primary_key=True, comment='文章ID')
-    account_id    = Column(Integer, comment='发布者账号ID')
-    title         = Column(String(99), comment='文章标题')
-    summary       = Column(Text, comment='文章概要')
-    content       = Column(Text, comment='文章主体内容')
-    word_count    = Column(Integer, comment='文章字数')
-    read          = Column(Integer, comment='阅读次数')
-    category_id   = Column(Integer, comment='所属分类')
-    tags          = Column(JSON, comment='标签')
+    __tablename__    = 'articles'
+    id               = Column(Integer, primary_key=True, comment='文章ID')
+    account_id       = Column(Integer, comment='发布者账号ID')
+    title            = Column(String(99), comment='文章标题')
+    summary          = Column(Text, comment='文章概要')
+    content_type     = Column(Integer, comment='文章编辑内容类型，1-Markdown，2-富文本')
+    content          = Column(Text, comment='文章主体内容')
+    content_markdown = Column(Text, comment='Markdown 文本内容，当 content_type=1 时存在')
+    word_count       = Column(Integer, comment='文章字数')
+    read             = Column(Integer, comment='阅读次数')
+    category_id      = Column(Integer, comment='所属分类')
+    tags             = Column(JSON, comment='标签')
     # TODO: 待定，是否需要冗余
     # like          = Column(Integer, comment='点赞次数')
     # 缩略图
