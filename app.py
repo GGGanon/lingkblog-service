9e4fad07a6476f32d2d712fbbfa4afe4b1a327a5
JalanJiang <448300947@qq.com>
2019-06-11 22:30:09 +0800

    fix(cors): 补充跨域请求与请求头

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 @version: v1.0
 @Author: JalanJiang
 @Date: 2019-06-02 01:43:27
-@LastEditTime: 2019-06-10 09:48:56
+@LastEditTime: 2019-06-11 22:27:31
 '''
 from lingkblog import create_app
 from lingkblog.common.jwt_auth import JWTAuth
@@ -18,21 +18,21 @@ import sqlite3
 
 app = create_app()
 
-@app.before_request
-def check_auth_token():
-    special_route = ['/admin-api/users/token']
-    if request.path not in special_route:
-        access_token = request.headers.get('Authorization')
-        payload      = JWTAuth.decode_access_token(access_token)
-        account_id   = payload['data']['id']
+# @app.before_request
+# def check_auth_token():
+#     special_route = ['/admin-api/users/token']
+#     if request.path not in special_route:
+#         access_token = request.headers.get('Authorization')
+#         payload      = JWTAuth.decode_access_token(access_token)
+#         account_id   = payload['data']['id']
 
-        # 判断用户是否存在
-        account_obj = AccountModel.query.filter_by(id=account_id).first()
-        if account_obj is None:
-            raise APIException(err_key='invalid_token')
+#         # 判断用户是否存在
+#         account_obj = AccountModel.query.filter_by(id=account_id).first()
+#         if account_obj is None:
+#             raise APIException(err_key='invalid_token')
 
-        g.account_id  = account_id
-        g.account_obj = account_obj
+#         g.account_id  = account_id
+#         g.account_obj = account_obj
 
 @app.after_request
 def open_cors(response):
@@ -44,8 +44,8 @@ def open_cors(response):
     # TODO: 区分正式与测试环境
     response = make_response(response)
     response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET,POST'
-    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PATCH,PUT,DELETE,OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type,Authorization'
     return response
         
 if __name__ == "__main__":
