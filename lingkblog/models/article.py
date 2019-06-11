914416812f630833465be5ee8a046de77367b5fc
JalanJiang <448300947@qq.com>
2019-06-11 11:08:14 +0800

    refactor(article): 文章表补充默认值

--- lingkblog/models/article.py
+++ lingkblog/models/article.py
@@ -3,7 +3,7 @@
 @version: v1.0
 @Author: JalanJiang
 @Date: 2019-06-07 11:12:59
-@LastEditTime: 2019-06-07 22:01:36
+@LastEditTime: 2019-06-11 11:00:09
 '''
 from sqlalchemy import Column, Integer, String, Text, JSON
 
@@ -20,9 +20,9 @@ class Article(Base, db.Model):
     content_type     = Column(Integer, comment='文章编辑内容类型，1-Markdown，2-富文本')
     content          = Column(Text, comment='文章主体内容')
     content_markdown = Column(Text, comment='Markdown 文本内容，当 content_type=1 时存在')
-    word_count       = Column(Integer, comment='文章字数')
+    word_count       = Column(Integer, default=0, comment='文章字数')
     read             = Column(Integer, default=0, comment='阅读次数')
-    category_id      = Column(Integer, comment='所属分类')
+    category_id      = Column(Integer, default=0, comment='所属分类。0-未分类')
     tags             = Column(JSON, comment='标签')
     # TODO: 待定，是否需要冗余
     # like          = Column(Integer, comment='点赞次数')
