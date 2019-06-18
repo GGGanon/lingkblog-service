1ddd0cb52650722cc6eec1bed455c6f3af6f8d97
JalanJiang <448300947@qq.com>
2019-06-18 17:36:39 +0800

    refactor(account): 改写账号路由

--- lingkblog/routes/admin/account.py
+++ lingkblog/routes/admin/account.py
@@ -3,7 +3,7 @@
 @version: v1.0
 @Author: JalanJiang
 @Date: 2019-06-02 15:25:56
-@LastEditTime: 2019-06-07 21:54:33
+@LastEditTime: 2019-06-18 17:36:16
 '''
 from flask import Blueprint, request
 from lingkblog.services.admin.account import Account as AccountService
@@ -30,3 +30,58 @@ def handle_accounts():
     if request.method == 'DELETE':
         return
     # return "Hello, LingKBlog Account!"
+
+@bp.route('', methods=['GET'])
+def get_accounts():
+    '''
+    @description: 获取账号列表
+    @param : 
+    @return: 账号列表
+    '''
+    pass
+
+@bp.route('/<int:id>', methods=['GET'])
+def get_account(id):
+    '''
+    @description: 获取指定账号信息
+    @param : 
+    @return: 
+    '''
+    pass
+
+@bp.route('', methods=['POST'])
+def add_account():
+    '''
+    @description: 注册新用户
+    @param : 
+    @return: 新注册用户资料
+    '''
+    account_service = AccountService(request)
+    return account_service.store()
+
+@bp.route('/<int:id>', methods=['PUT'])
+def update_account(id):
+    '''
+    @description: 修改账号资料
+    @param : 
+    @return: 
+    '''
+    pass
+
+@bp.route('/<int:id>', methods=['PATCH'])
+def update_part_account(id):
+    '''
+    @description: 修改账号部分资料
+    @param : 
+    @return: 
+    '''
+    pass
+
+@bp.route('/<int:id>', methods=['DELETE'])
+def delete_account(id):
+    '''
+    @description: 删除账号
+    @param : 
+    @return: 
+    '''
+    pass
