6f7e3cc072dc530f751de71946c2d7ea633fea71
JalanJiang <448300947@qq.com>
2019-06-23 23:16:55 +0800

    feat(article): 文章接口改为根据 uuid 定位具体文章
    1. 获取文章详情接口
    2. 更新文章接口（PUT/PATCH）
    3. 删除文章接口

--- lingkblog/routes/admin/article.py
+++ lingkblog/routes/admin/article.py
@@ -3,7 +3,7 @@
 @version: v1.0
 @Author: JalanJiang
 @Date: 2019-06-07 21:42:59
-@LastEditTime: 2019-06-18 16:55:03
+@LastEditTime: 2019-06-23 23:15:18
 '''
 from flask import Blueprint, request
 from lingkblog.services.admin.article import Article as ArticleService
@@ -16,27 +16,27 @@ def get_articles():
     article = ArticleService(request)
     return article.index()
 
-@bp.route('/<int:id>', methods=['GET'])
-def get_article(id):
+@bp.route('/<path:uuid>', methods=['GET'])
+def get_article(uuid):
     article = ArticleService(request)
-    return article.show(id)
+    return article.show(uuid)
 
 @bp.route('', methods=['POST'])
 def add_article():
     article = ArticleService(request)
     return article.store()
 
-@bp.route('/<int:id>', methods=['PUT'])
-def update_article(id):
+@bp.route('/<path:uuid>', methods=['PUT'])
+def update_article(uuid):
     article = ArticleService(request)
-    return article.update(id)
+    return article.update(uuid)
 
-@bp.route('/<int:id>', methods=['PATCH'])
-def update_part_article(id):
+@bp.route('/<path:uuid>', methods=['PATCH'])
+def update_part_article(uuid):
     article = ArticleService(request)
-    return article.partical_update(id)
+    return article.partical_update(uuid)
 
-@bp.route('/<int:id>', methods=['DELETE'])
-def delete_article(id):
+@bp.route('/<path:uuid>', methods=['DELETE'])
+def delete_article(uuid):
     article = ArticleService(request)
-    return article.delete(id)
+    return article.delete(uuid)
